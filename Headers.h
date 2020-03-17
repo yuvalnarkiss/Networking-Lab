2ff71d6ff36aa2a17d42a22e68ac0d5c92d590b7
yuvalnarkiss <62073438+yuvalnarkiss@users.noreply.github.com>
2020-03-17 20:22:16 +0200

    Update Headers.h

--- Headers.h
+++ Headers.h
@@ -9,6 +9,11 @@
 #define TIM2_BASE             (APB1PERIPH_BASE + 0x0000)
 #define TIM3_BASE             (APB1PERIPH_BASE + 0x0400)
 #define TIM4_BASE             (APB1PERIPH_BASE + 0x0800)
+#define GPIOC_BASE_ADDRESS    0x40011000
+#define CRH                   0x04
+#define RCC_BASE_ADDRESS      0x40021000
+#define AHBENR                0x14
+#define APB2ENR               0x18
 #define USART1BASE            0x40013800
 #define CR1                   0x0C
 #define CR2                   0x10
