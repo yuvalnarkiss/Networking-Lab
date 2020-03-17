1a752aaf188aace5da7692cd8c247ac8d5a54918
yuvalnarkiss <62073438+yuvalnarkiss@users.noreply.github.com>
2020-03-17 19:13:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
+void configRegister(){
+	
+	
+	
+}
+
+
+
 int main(void)
 {
+	configRegister();
+	int send_one = 0;
+	int send_zero = 0;
 	while(1)
 	{
+		/*Transmission Area*/
+		if (send_one == 1)
+		{
+			/*Write 1 to TX register*/
+		}
+		if (send_zero == 1)
+		{
+			/*Write 0 to TX register*/
+		}
+		
+		
+		/*Recieve Area*/
+		
+		
+		
 	}
 }
  
+
+
+
