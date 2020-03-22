657773d5c0182341fbe04839a802132742fbaaca
yuvalnarkiss <62073438+yuvalnarkiss@users.noreply.github.com>
2020-03-22 22:16:59 +0200

    Update Headers.h

--- Headers.h
+++ Headers.h
@@ -7,7 +7,8 @@
 #define CR1                   0x0C
 #define CR2                   0x10
 #define CR3                   0x14
-#define USART1BASE_CR1        (USART1BASE + CR1)
+#define BRR									0x08
+#define DR										0x04
 
 
 void configRegister();
